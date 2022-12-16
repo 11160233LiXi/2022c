b1cf6a7dbe15b51abfe9a00b686842c494e6795b
11160233 <11160233@me.mcu.edu.tw>
2022-12-16 11:58:19 +0800

    week15

---
+++ week15/week15-01.cpp
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+int main()
+{
+    printf("hello world\n");
+    char line[]="hello world";
+
+    printf("整數 %d\n",100);
+    printf("福點數 %f\n",3.141592653);
+    printf("%s 字串\n",line);
+}
---
+++ week15/week15-02.cpp
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main()
+{
+    printf("%c : %d\n",65,65);
+    printf("%c : %d\n",66,66);
+    printf("%c : %d\n",67,67);
+    printf("%c : %d\n",'A','A');
+    printf("%c : %d\n",'B','B');
+    printf("%c : %d\n",'C','C');
+    printf("數字用64和單引號用A結果一樣\n");
+    printf("%c : %d\n",97,97);
+    printf("%c : %d\n",'a','a');
+}
+///32最適合裝///32+32+32+32
+///看不到 符號 大寫 小小
+///A:65 a:97
